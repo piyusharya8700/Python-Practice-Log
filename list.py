0edbcfeb8e5880ae4514168b47584c183fe81beb
piyusharya8700 <piyusharya3003@proton.me>
2026-07-11 23:40:00 +0530

    practicing list in python

---
+++ list.py
@@ -0,0 +1,23 @@
+l=[10,20,30,40]
+
+nums=[1,2,3,4,5]
+for i in nums:
+    print(i)
+
+total=sum(l)
+print(total)
+
+largest=max(l)
+print(largest)
+
+leest=[2,3,4,5,6,7]
+leest.reverse()
+print(leest)
+
+
+least=[1,2,3,4,4,4,3,3,1,1,1,1]
+count1=least.count(1)
+print(count1)
+
+rmvdup=list(set(least))
+print(rmvdup)
