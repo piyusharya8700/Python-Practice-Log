e097f927451dd1205c05348c5609e27c57f54a03
piyusharya8700 <piyusharya3003@proton.me>
2026-07-14 23:46:01 +0530

    practicing while loops its kinda interesting

---
+++ whileloop.py
@@ -0,0 +1,33 @@
+num=1
+while num <= 10:
+    print(num)
+    num+=1
+
+num=0
+while num <= 20:
+    print(num)
+    num+=2
+
+num=10
+while num >= 1:
+    print(num)
+    num-=1
+
+sum=0
+num=1
+while num <= 100:
+    sum+=num
+    num+=1
+print(sum)
+
+num=7
+while num<=70:
+    print(num)
+    num+=7
+
+m=1
+num=5
+while num >= 1:
+    m*=num
+    num-=1
+print(m)
