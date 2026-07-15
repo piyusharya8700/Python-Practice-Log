261f5ac02b36f24f1ef2ccfc7e7eeaf07568684f
piyusharya8700 <piyusharya3003@proton.me>
2026-07-15 23:36:10 +0530

    practicing tuples

---
+++ tuple.py
@@ -0,0 +1,22 @@
+t=(10,20,30,40)
+print(t)
+
+print(t[0])
+print(t[-4])
+print(t[3])
+print(t[-1])
+
+print(len(t))
+
+a, b, c, d= t
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(t.index(30))
+
+nums=(1,2,3,4)
+for i in nums:
+    print(i)
+
